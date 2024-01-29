8dc972baa85a9c11ad3654938d80ddc378d974bb
Joshua Pearson <jpearson@ximasoftware.com>
2024-01-29 12:04:32 -0700

    divider blur

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,11 +1,11 @@
 export const HorizontalDivider = () => {
   return (
-    <div className="h-0.5 w-4/6 justify-self-center bg-gradient-to-r from-slate-500/5 via-slate-500 to-slate-500/5"></div>
+    <div className="h-0.5 w-4/6 justify-self-center bg-gradient-to-r from-slate-500/5 via-slate-400 to-slate-500/5 blur-[1px]"></div>
   );
 };
 
 export const VerticalDivider = () => {
   return (
-    <div className="h-0.5 w-4/6 self-center bg-gradient-to-b from-slate-500/5 via-slate-500 to-slate-500/5"></div>
+    <div className="h-0.5 w-4/6 self-center bg-gradient-to-b from-slate-500/5 via-slate-400 to-slate-500/5 blur-[1px]"></div>
   );
 };
